c1202419d7d2f8c5a8cfa10bce508d0d2c7f2c5a
Nazariy Shypka <shypkanazar22@gmail.com>
2023-12-03 19:19:43 +0200

    added data receiving functionality

--- Server.cpp
+++ Server.cpp
@@ -3,10 +3,88 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 #include <Windows.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#define TEMPERATURE -3
+
+#define STOCK_PRICE_MICROSOFT 374.51
+#define STOCK_PRICE_GOOGLE 131.86
+#define STOCK_PRICE_APPLE 191.24
+
+#define EXCHANGE_DOLLAR_RATE 38.3
+#define EXCHANGE_EURO_RATE 40.5
+#define EXCHANGE_ZLOTY_RATE 9.3
 
 using namespace std;
 
 const int PORT = 12345;
+
+
+void writeToFile(const string& fileName, const string& data) {
+    ofstream file(fileName, ios_base::app);
+    if (file.is_open()) {
+        file << __TIME__ + '\n' + data + '\n';
+        file.close();
+    }
+}
+double getRandomValue(int randomInterval) {
+    double sign = (rand() % 2 == 0) ? 1.0 : -1.0;
+    return sign * (static_cast<double>(rand() % randomInterval) + static_cast<double>(rand() % 100) / 100);
+}
+
+string randomWeatherForecast() {
+    const int RANDOM_INTERVAL = 3;
+
+    double temperature = TEMPERATURE + getRandomValue(RANDOM_INTERVAL);
+    string str = " Temperature:  : " + to_string(temperature) + " °C";
+
+    writeToFile("Weather.txt", str);
+
+    return str;
+}
+string randomSharePrice() {
+    const int RANDOM_INTERVAL = 3;
+
+    double microsoftShare = STOCK_PRICE_MICROSOFT + getRandomValue(RANDOM_INTERVAL);
+    double googleShare = STOCK_PRICE_GOOGLE + getRandomValue(RANDOM_INTERVAL);
+    double appleShare = STOCK_PRICE_APPLE + getRandomValue(RANDOM_INTERVAL);
+
+    string str = " Shares Microsoft:  : " + to_string(microsoftShare) +
+        "\n Shares Google:  : " + to_string(googleShare) +
+        "\n Shares Apple:  : " + to_string(appleShare);
+
+    writeToFile("Shares.txt", str);
+
+    return str;
+}
+string randomExchangeRate() {
+    const int RANDOM_INTERVAL = 5;
+
+    double dollarRate = EXCHANGE_DOLLAR_RATE + getRandomValue(RANDOM_INTERVAL);
+    double euroRate = EXCHANGE_EURO_RATE + getRandomValue(RANDOM_INTERVAL);
+    double zlotyRate = EXCHANGE_ZLOTY_RATE + getRandomValue(RANDOM_INTERVAL/2);
+
+    string str = " Dollar rate : " + to_string(dollarRate) +
+        "\n Euro rate : " + to_string(euroRate) +
+        "\n Zloty rate : " + to_string(zlotyRate);
+
+    writeToFile("Rate.txt", str);
+
+    return str;
+}
+
+DWORD WINAPI sendWeatherForecast(PVOID pvParam) {
+
+}
+DWORD WINAPI sendSharePrice(PVOID pvParam) {
+
+}
+DWORD WINAPI sendExchangeRate(PVOID pvParam) {
+
+}
 
 int main() {
 
