acc7ba4b68752489186f7b24617600ec4c6eda08
Nazariy Shypka <nazarii.shypka.pz.2022@lpnu.ua>
2023-12-03 22:36:01 +0200

    server optimization

--- utilityFunctions.cpp
+++ utilityFunctions.cpp
@@ -1,16 +1,94 @@
 #include "utilityFunctions.h"
-#include <string>
-using namespace std;
 
 extern HANDLE mutex;
 extern string sharesRate;
 extern string exchangeRate;
 extern string weatherForecast;
 
+
+void writeToFile(const string& fileName, const string& data) {
+    ofstream file(fileName, ios_base::app);
+    if (file.is_open()) {
+        file << __TIME__ + '\n' + data + '\n';
+        file.close();
+    }
+}
+double getRandomValue(int randomInterval) {
+    double sign = (rand() % 2 == 0) ? 1.0 : -1.0;
+    return sign * (static_cast<double>(rand() % randomInterval) + static_cast<double>(rand() % 100) / 100);
+}
+
+string randomWeatherForecast() {
+    const int RANDOM_INTERVAL = 3;
+
+    double temperature = TEMPERATURE + getRandomValue(RANDOM_INTERVAL);
+    string str = " Temperature:  : " + to_string(temperature) + " В°C";
+
+    writeToFile("Weather.txt", str);
+
+    return str;
+}
+string randomSharePrice() {
+    const int RANDOM_INTERVAL = 3;
+
+    double microsoftShare = STOCK_PRICE_MICROSOFT + getRandomValue(RANDOM_INTERVAL);
+    double googleShare = STOCK_PRICE_GOOGLE + getRandomValue(RANDOM_INTERVAL);
+    double appleShare = STOCK_PRICE_APPLE + getRandomValue(RANDOM_INTERVAL);
+
+    string str = " Shares Microsoft:  : " + to_string(microsoftShare) +
+        "\n Shares Google:  : " + to_string(googleShare) +
+        "\n Shares Apple:  : " + to_string(appleShare);
+
+    writeToFile("Shares.txt", str);
+
+    return str;
+}
+string randomExchangeRate() {
+    const int RANDOM_INTERVAL = 5;
+
+    double dollarRate = EXCHANGE_DOLLAR_RATE + getRandomValue(RANDOM_INTERVAL);
+    double euroRate = EXCHANGE_EURO_RATE + getRandomValue(RANDOM_INTERVAL);
+    double zlotyRate = EXCHANGE_ZLOTY_RATE + getRandomValue(RANDOM_INTERVAL / 2);
+
+    string str = " Dollar rate : " + to_string(dollarRate) +
+        "\n Euro rate : " + to_string(euroRate) +
+        "\n Zloty rate : " + to_string(zlotyRate);
+
+    writeToFile("Rate.txt", str);
+
+    return str;
+}
+
+DWORD WINAPI processClientRequests(LPVOID param) {
+    SOCKET clientSocket = *reinterpret_cast<SOCKET*>(param);
+
+    while (true) {
+        char buffer[1024];
+        memset(buffer, 0, sizeof(buffer));
+
+        // Get data from client
+        if (recv(clientSocket, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
+            cerr << "Receive failed" << endl;
+            closesocket(clientSocket);
+            continue;
+        }
+
+        cout << " " << __TIME__ << " Received message from client: " << buffer << std::endl;
+
+        
+        //Sending a response to the client
+        const char* response = "Server received your message.";
+        send(clientSocket, response, strlen(response), 0);
+    }
+    closesocket(clientSocket);
+
+    return 0;
+}
+
 DWORD WINAPI updateWeatherForecast(LPVOID param) {
     while (1) {
         WaitForSingleObject(mutex, INFINITE);
-        //weatherForecast = generateWeatherForecast(); //імплементацію треба
+        //weatherForecast = generateWeatherForecast(); //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
         ReleaseMutex(mutex);
         Sleep(7000);
     }
@@ -19,7 +97,7 @@ DWORD WINAPI updateWeatherForecast(LPVOID param) {
 DWORD WINAPI updateExchangeRate(LPVOID param) {
     while (1) {
         WaitForSingleObject(mutex, INFINITE);
-        //exchangeRate = generateExchangeRate(); //імплементацію треба
+        //exchangeRate = generateExchangeRate(); //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
         ReleaseMutex(mutex);
         Sleep(6000);
     }
@@ -28,9 +106,9 @@ DWORD WINAPI updateExchangeRate(LPVOID param) {
 DWORD WINAPI updateSharesRate(LPVOID param) {
     while (1) {
         WaitForSingleObject(mutex, INFINITE);
-        //sharesRate = generateSharesRate(); //імплементацію треба
+        //sharesRate = generateSharesRate(); //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
         ReleaseMutex(mutex);
-        Sleep(5000); //час хай буде менший поки. Оновлюємо інфу кожні n мс.
+        Sleep(5000); //пїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ. пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ n пїЅпїЅ.
     }
     return 0;
 }
--- utilityFunctions.h
+++ utilityFunctions.h
@@ -1,6 +1,29 @@
 #pragma once
+#include <iostream>
 #include <Windows.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
+#define TEMPERATURE -3
+
+#define STOCK_PRICE_MICROSOFT 374.51
+#define STOCK_PRICE_GOOGLE 131.86
+#define STOCK_PRICE_APPLE 191.24
+
+#define EXCHANGE_DOLLAR_RATE 38.3
+#define EXCHANGE_EURO_RATE 40.5
+#define EXCHANGE_ZLOTY_RATE 9.3
+
+DWORD WINAPI processClientRequests(LPVOID);
 DWORD WINAPI updateWeatherForecast(LPVOID);
 DWORD WINAPI updateExchangeRate(LPVOID);
 DWORD WINAPI updateSharesRate(LPVOID);
+
+void writeToFile(const string& fileName, const string& data);
+double getRandomValue(int randomInterval);
+
+string randomWeatherForecast();
+string randomSharePrice();
+string randomExchangeRate();
