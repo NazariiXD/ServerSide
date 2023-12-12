52f9495486409f99cb2b8cf2cf00dbc37dfdeb6e
Nazarii Shypka <145384090+NazariiXD@users.noreply.github.com>
2023-12-12 13:46:16 +0200

    added obtaining time functionality, changed getRandom function and data fetch functions, optimization (#6)

--- Server.cpp
+++ Server.cpp
@@ -11,13 +11,13 @@ int main() {
 	// Sockets library initialisation
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Failed to initialize Winsock!\n";
+        std::cerr << "Failed to initialize Winsock!" << std::endl;
         return -1;
     }
     sockaddr_in serverAddress{};
     // Use inet_pton to convert an IP address from a string representation
     if (inet_pton(AF_INET, SERVER_IP, &(serverAddress.sin_addr)) != 1) {
-        std::cerr << "Invalid IP address\n";
+        std::cerr << "Invalid IP address" << std::endl;
         WSACleanup();
         return -1;
     }
@@ -30,13 +30,13 @@ int main() {
     // NULL - default TCP protocol
     SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, NULL);
     if (serverSocket == INVALID_SOCKET) {
-        std::cerr << "Error creating socket. Error code : " << WSAGetLastError() << "\n";
+        std::cerr << "Error creating socket. Error code : " << WSAGetLastError() << std::endl;
         WSACleanup();
         return -1;
     }
     // Binding for accepting connections
     if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
-        std::cerr << "Bind failed. Error code : " << WSAGetLastError() << "\n";
+        std::cerr << "Bind failed. Error code : " << WSAGetLastError() << std::endl;
         closesocket(serverSocket);
         WSACleanup();
         return -1;
@@ -44,33 +44,33 @@ int main() {
 
     mutex = CreateMutex(NULL, FALSE, NULL);
     if (mutex == NULL) {
-        std::cerr << "Failed to create mutex\n";
+        std::cerr << "Failed to create mutex" << std::endl;
         return -1;
     }
     if (CreateThread(NULL, NULL, sendWeatherForecast, NULL, NULL, NULL) == NULL) {
-        std::cerr << "Failed to launch forecast subscription service\n";
+        std::cerr << "Failed to launch forecast subscription service" << std::endl;
         return -1;
 
     }
     if (CreateThread(NULL, NULL, sendExchangeRate, NULL, NULL, NULL) == NULL) {
-        std::cerr << "Failed to launch exchange rate subscription service\n";
+        std::cerr << "Failed to launch exchange rate subscription service" << std::endl;
         return -1;
     }
     if (CreateThread(NULL, NULL, sendSharePrice, NULL, NULL, NULL) == NULL) {
-        std::cerr << "Failed to launch shares rate subscription service\n";
+        std::cerr << "Failed to launch shares rate subscription service" << std::endl;
         return -1;
     }
     std::cout << "Database connected\n";
 
     // Waiting for connections
     if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cerr << "Listen failed\n";
+        std::cerr << "Listen failed" << std::endl;
         closesocket(serverSocket);
         WSACleanup();
         return -1;
     }
 
-    std::cout << "Server listening on port: " << PORT << "...\n";
+    std::cout << "Server listening on port: " << PORT << "..." << std::endl;
 
     // Creating client socket
     SOCKET clientSocket;
@@ -82,15 +82,15 @@ int main() {
 
         clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &client_addr_size);
         if (clientSocket == INVALID_SOCKET) {
-            std::cerr << "Accept failed. Error code : " << WSAGetLastError() << "\n";
+            std::cerr << "Accept failed. Error code : " << WSAGetLastError() << std::endl;
             continue;
         }
 
-        std::cout << " " << __TIME__ << " Client connected\n";
+        std::cout << " " << getCurrentTime() << " Client connected" << std::endl;
 
         //Processing of client requests
         if (CreateThread(NULL, 0, manageClientSubscription, &clientSocket, 0, NULL) == NULL) {
-            std::cerr << "Failed to launch shares rate subscription service\n";
+            std::cerr << "Failed to launch shares rate subscription service" << std::endl;
             closesocket(clientSocket);
         }
     }
